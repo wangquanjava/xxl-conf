d9b5d0b4c5626197ac5008c456463a0a19fa5349
wqquan.wang <wqquan.wang@qunar.com>
2017-12-22 19:41:48 +0800

    编码

--- xxl-conf-example/src/main/java/com/xxl/conf/example/controller/IndexController.java
+++ xxl-conf-example/src/main/java/com/xxl/conf/example/controller/IndexController.java
@@ -10,13 +10,13 @@ import javax.annotation.Resource;
 
 @Controller
 public class IndexController {
-	
-	@Resource
-	private Configuration configuration;
-	
-	@RequestMapping("")
-	@ResponseBody
-	public String index(){
+
+    @Resource
+    private Configuration configuration;
+
+    @RequestMapping("")
+    @ResponseBody
+    public String index() {
 
         /**
          * 方式1: XML文件中的占位符方式
@@ -32,10 +32,10 @@ public class IndexController {
          * 说明: API方式获取, 只需要执行diamante "XxlConfClient.get("key", null)" 即可, 在业务中使用比较方便 ,而且接受XXL-CONF实时推送更新。 同时因为底层有配置缓存,并不存在性能问题;
          *
          */
-		String paramByClient = XxlConfClient.get("default.key02", null);
+        String paramByClient = XxlConfClient.get("default.key02", null);
 
-		String result = "XML:<hr>default.key01=" + paramByXml;
-		result += "<br><br><br>API:<hr>default.key02=" + paramByClient;
-		return result;
-	}
+        String result = "XML:<hr>default.key01=" + paramByXml;
+        result += "<br><br><br>API:<hr>default.key02=" + paramByClient;
+        return result;
+    }
 }
